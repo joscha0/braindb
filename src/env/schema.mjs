e03fac29696c6d9ee03cc77ffcc856c76ec284fd
Joscha <joschavonandrian@gmail.com>
2023-02-04 23:38:37 +0100

    fix env

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,21 +6,11 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  DATABASE_URL: z.string().url(),
+  REACT_APP_ENDPOINT: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
-  NEXTAUTH_SECRET:
-    process.env.NODE_ENV === "production"
-      ? z.string().min(1)
-      : z.string().min(1).optional(),
-  NEXTAUTH_URL: z.preprocess(
-    // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-    // Since NextAuth.js automatically uses the VERCEL_URL if present.
-    (str) => process.env.VERCEL_URL ?? str,
-    // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
-  ),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
+
+  REACT_APP_PROJECT: z.string(),
+  REACT_APP_DATABASE_ID: z.string(),
 });
 
 /**
@@ -29,12 +19,10 @@ export const serverSchema = z.object({
  * @type {{ [k in keyof z.input<typeof serverSchema>]: string | undefined }}
  */
 export const serverEnv = {
-  DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
-  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-  NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+  REACT_APP_ENDPOINT: process.env.REACT_APP_ENDPOINT,
+  REACT_APP_PROJECT: process.env.REACT_APP_PROJECT,
+  REACT_APP_DATABASE_ID: process.env.REACT_APP_DATABASE_ID,
 };
 
 /**
